e7f2cdc287a3457441ec13fe38e3cb94cd578933
indelgarcia <indelg7@gmail.com>
2024-09-16 02:36:36 -0300

    Adding Santi's Homepages (fix later). Adding movie reviews and details 'when clicked' pages. Also hyperlinked them in relavent pages

---
+++ js/movie_details.js
@@ -0,0 +1,41 @@
+// Get modal elements
+var modal = document.getElementById("trailer-modal");
+var trailerTrigger = document.getElementById("trailer-trigger");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the movie poster, open the modal
+trailerTrigger.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on the close button, close the modal
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Tab functionality
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Set default active tab
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.tab button').click();
+});
