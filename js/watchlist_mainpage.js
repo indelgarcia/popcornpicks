aa6f503a247e0ca89badfeed712736550029786b
indelgarcia <indelg7@gmail.com>
2024-10-07 02:22:04 -0300

    Refactor: Update watchlist_mainpage.js for consistent variable names and better comments

--- js/watchlist_mainpage.js
+++ js/watchlist_mainpage.js
@@ -1,24 +1,25 @@
-// JS for managing the watchlist functionalities
+//watchlist_mainpage.js
+
+// get view elements and buttons
 document.addEventListener('DOMContentLoaded', () => {
     const yourWatchlistView = document.getElementById('your-watchlist-view');
     const discoverPicksView = document.getElementById('discover-picks-view');
     const yourWatchlistBtn = document.getElementById('your-watchlist-btn');
     const discoverPicksBtn = document.getElementById('discover-picks-btn');
-    const postWatchlistBtn = document.getElementById('post-watchlist-btn');
 
-    // Switch to "Your Watchlist" View
+    // switch to "Your Watchlist" View
     yourWatchlistBtn.addEventListener('click', () => {
         discoverPicksView.style.display = 'none';
+        discoverPicksBtn.classList.remove('active');
         yourWatchlistView.style.display = 'block';
         yourWatchlistBtn.classList.add('active');
-        discoverPicksBtn.classList.remove('active');
     });
 
-    // Switch to "Discover Picks" View
+    // switch to "Discover Picks" View
     discoverPicksBtn.addEventListener('click', () => {
-        yourWatchlistView.style.display = 'none';
         discoverPicksView.style.display = 'block';
         discoverPicksBtn.classList.add('active');
+        yourWatchlistView.style.display = 'none';
         yourWatchlistBtn.classList.remove('active');
     });
 });
