8e03bf4d2f41a04ed017d81765dca33dd03b8f25
indelgarcia <indelg7@gmail.com>
2024-10-07 02:17:24 -0300

    Refactor: Update movie_details.js for consistent variable names and better comments

--- js/movie_details.js
+++ js/movie_details.js
@@ -1,3 +1,5 @@
+// movie_details.js
+
 // get modal elements
 var modal = document.getElementById("image-modal");
 var imageTrigger = document.getElementById("image-trigger");
@@ -6,7 +8,7 @@ var closeBtn = document.getElementsByClassName("close")[0];
 
 // when the user clicks on the movie poster, open the modal
 imageTrigger.onclick = function() {
-    // Get the source of the movie poster and display it in the modal
+    // get the source of the movie poster and display it in the modal
     enlargedImage.src = document.getElementById("movie-image").src;
     modal.style.display = "block";
 }
@@ -23,22 +25,33 @@ window.onclick = function(event) {
     }
 }
 
-// Tab functionality
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
+// tab function to switch between different tab sections
+function openTab(tabClickEvent, tabName) {
+    //init variables
+    let tabcontent, tablinks;
+
+    // get all elements with class "tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none"; // hide all tab contents
     }
+
+    // get all elements with class "tablinks" and remove the "active" class
     tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", ""); // remove "active" from all tabs
     }
+
+    // show the selected tab's content
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+
+    // add the "active" class to the clicked tab
+    tabClickEvent.currentTarget.className += " active";
 }
 
-// Set default active tab
+// set default active tab when the page loads
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.tab button').click();
+    document.querySelector('.tab button').click(); // Simulate a click on the first tab button
 });
+
+
